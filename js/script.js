19b1965337c2ffe8d34f776088700beee82687bc
JFajri <jfnew24@gmail.com>
2026-09-02 11:48:01 +0700

    Update visual Project Card dan layout gallery premium.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const projects = [
+    {
+        title: "DevFlow",
+        category: "Web",
+        year: 2026,
+        description: "...",
+        technologies: ["HTML", "CSS", "JavaScript"]
+    },
+
+    {
+        title: "Nebula",
+        category: "Mobile",
+        year: 2026,
+        description: "...",
+        technologies: ["Flutter", "Dart"]
+    }
+];
